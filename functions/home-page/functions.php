fb3bccaf7a6ca10dbe229f9ee033fbfde8059c0b
Timofey Krivenkov <drake9100@mail.ru>
2023-08-07 02:21:55 +0300

    ADD: Секции шапки и hero

---
+++ functions/home-page/functions.php
@@ -0,0 +1,18 @@
+<?php
+
+add_filter( 'template_include', function ( $template ) {
+	return is_front_page() ? locate_template( '/templates/home-page/home-page.php' ) : $template;
+}, 99 );
+
+if( function_exists('acf_add_options_page') ) {
+	
+	acf_add_options_page(array(
+		'page_title' 	=> 'Настройки Главной',
+		'menu_title'	=> 'Настройки Главной',
+		'menu_slug' 	=> 'theme-general-settings',
+		'capability'	=> 'edit_posts',
+		'redirect'		=> false,
+    'position'    => 2
+	));
+	
+}
