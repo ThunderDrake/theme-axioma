bd6bfab7945c7e2e8eb9c69519ab18bf2d9d5c16
Timofey Krivenkov <drake9100@mail.ru>
2023-08-18 12:05:28 +0300

    ADD: Секция о компании (верстка)

--- templates/home-page/home-page.php
+++ templates/home-page/home-page.php
@@ -4,6 +4,8 @@ ct()->header();
 <?php
 ct()->template('home-page/parts/hero-section.php');
 ct()->template('home-page/parts/services.php');
+ct()->template('home-page/parts/about.php');
+ct()->template('home-page/parts/form-section.php');
 ?>
 <?php
 ct()->footer();
---
+++ templates/home-page/parts/about.php
@@ -0,0 +1,19 @@
+<section class="about">
+  <div class="about__container container">
+    <div class="about__content-wrapper">
+      <img loading="lazy" src="<?= ct()->get_static_url() ?>/img/logo.svg" class="about__logo" width="640" height="190" alt="Axioma logo">
+      <p class="about__text">– это частный медицинский центр, в котором осуществляется качественная диагностика на экспертном оборудовании, проводятся амбулаторные приемы специалистов - гинеколога-эндокринолога, гастроэнтеролога, онколога, невролога.</p>
+      <p class="about__text">Мы работаем на основе интегративного подхода и превентивной медицины, гармонично сочетаем личный опыт  врача с достижениями медицинской науки.</p>
+    </div>
+    <div class="about__company">
+      <picture>
+        <source srcset="<?= ct()->get_static_url() ?>/img/about/building.webp" type="image/webp">
+        <img loading="lazy" src="<?= ct()->get_static_url() ?>/img/about/building.jpg" class="about__company-image" width="443" height="443" alt="Вход в здание офиса Аксиома здоровья">
+      </picture>
+      <div class="about__company-content">
+        <p>Наши пациенты получают высококвалифицированную медицинскую помощь в сочетании с внимательным отношением персонала. </p>
+        <p>Мы обеспечили удобный график работы и комфортный сервис</p>
+      </div>
+    </div>
+  </div>
+</section>
